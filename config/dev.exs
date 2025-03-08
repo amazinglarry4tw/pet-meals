4267b762c164bca373628c8e9508aad9ce3d5553
Jeremy Gallagher <jeremy@highgatestudios.net>
2025-03-07 22:47:55 -0500

    Updated config to allow connections from the network.

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :pet_meals, PetMeals.Repo,
 # Binding to loopback ipv4 address prevents access from other machines.
 config :pet_meals, PetMealsWeb.Endpoint,
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4002],
+  http: [ip: {0, 0, 0, 0}, port: 4002],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
